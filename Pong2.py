87d8144ed5c6373e0e857ab4604b6ac0bcc8bbc2
Jesse <jldohmann@utexas.edu>
2020-07-12 15:23:44 -0500

    Add files via upload
    initial files for pong

---
+++ Pong2.py
@@ -0,0 +1,36 @@
+# Simple Pong in Python 3
+
+import turtle
+
+wn = turtle.Screen()
+wn.title("Pong by @jldohmann")
+wn.bgcolor("black")
+wn.setup(width=800, height=600)
+wn.tracer(0)
+
+# Paddle A
+paddle_a = turtle.Turtle()
+paddle_a.speed(0)
+paddle_a.shape("square")
+paddle_a.color("white")
+paddle_a.shapesize(stretch_wid=5, stretch_len=1)
+paddle_a.penup()
+paddle_a.goto(-350, 0)
+
+
+# Paddle B
+paddle_b = turtle.Turtle()
+paddle_b.speed(0)
+paddle_b.shape("square")
+paddle_b.color("white")
+paddle_b.shapesize(stretch_wid=5, stretch_len=1)
+paddle_b.penup()
+paddle_b.goto(350, 0)
+
+
+
+# Ball
+
+# Main game loop
+while True:
+    wn.update()
